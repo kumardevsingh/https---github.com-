8bf6c94636b7837776f740efadea49dcd2011a49
Devendra Kumar <kumardevsingh@gmail.com>
2021-04-02 15:23:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function countLetters(str) {
+    let tempArr = str.split('');
+
+    tempArr = tempArr.sort((a, b) => {
+        return a > b ? 1 : -1;
+    })
+
+    let letters = [];
+    let count = 1;
+
+    for (let i = 0; i < tempArr.length; i++) {
+        if (tempArr[i] === tempArr[i + 1]) {
+            count++;
+        } else {
+            let value = `${count}${tempArr[i]}`;
+            letters = [...letters, value];
+            count = 1;
+        }
+
+    }
+
+    return letters.join('');
+
+}
+console.log(countLetters('aabbbcccccddefrraaa'));
